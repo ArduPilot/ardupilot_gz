73e352667d068d893d3369432fe32f7c1c6844fb
Rhys Mainwaring <rhys.mainwaring@me.com>
2023-09-16 12:17:14 +0100

    Copter: fix odometry transform
    Update iris_with_lidar model
    - Remove PosePublisher plugin from iris_with_lidar model.
    - Set base_link as mobile base in odometry publisher.
    
    Update iris_maze world
    - Change initial pose to align with world x-axis (east).
    - For consistency with cartographer.
    
    Update iris_bridge config, prefix TF topics from Gazebo with /gz.
    
    Update iris_maze launch file
    - Update comment for robot_state_publisher in launch.
    - Add package dependency on topic_tools.
    - Remove tf remapping in robot_state_publisher.
    - Add topic_tools node to relay /filter '/gz/tf' for odom transform.
    - Add launch argument to enable relay of /gz/tf to /tf (default true).
    - Add launch event to delay relay until bridge has started.
    
    Update rviz config
    - Remove 'iris' TF prefix from rviz.
    - Add laser scan to rviz.
    
    Signed-off-by: Rhys Mainwaring <rhys.mainwaring@me.com>

--- ardupilot_gz_bringup/launch/robots/iris.launch.py
+++ ardupilot_gz_bringup/launch/robots/iris.launch.py
@@ -40,12 +40,21 @@ import os
 from ament_index_python.packages import get_package_share_directory
 
 from launch import LaunchDescription
+from launch.actions import DeclareLaunchArgument
 from launch.actions import IncludeLaunchDescription
+from launch.actions import RegisterEventHandler
+
+from launch.conditions import IfCondition
+
+from launch.event_handlers import OnProcessStart
+
 from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch.substitutions import LaunchConfiguration
 from launch.substitutions import PathJoinSubstitution
 
 from launch_ros.actions import Node
 from launch_ros.substitutions import FindPackageShare
+
 
 
 def generate_launch_description():
@@ -122,7 +131,7 @@ def generate_launch_description():
         robot_desc = infp.read()
         # print(robot_desc)
 
-    # Remap the /tf and /tf_static under /ignore as the TF conflicts.
+    # Publish /tf and /tf_static.
     robot_state_publisher = Node(
         package="robot_state_publisher",
         executable="robot_state_publisher",
@@ -130,6 +139,7 @@ def generate_launch_description():
         output="both",
         parameters=[
             {"robot_description": robot_desc},
+            {"frame_prefix": ""},
         ],
     )
 
@@ -148,10 +158,52 @@ def generate_launch_description():
         output="screen",
     )
 
+    # Transform - use if the model includes "gz::sim::systems::PosePublisher"
+    #             and a filter is required.
+    # topic_tools_tf = Node(
+    #     package="topic_tools",
+    #     executable="transform",
+    #     arguments=[
+    #         "/gz/tf",
+    #         "/tf",
+    #         "tf2_msgs/msg/TFMessage",
+    #         "tf2_msgs.msg.TFMessage(transforms=[x for x in m.transforms if x.header.frame_id == 'odom'])",
+    #         "--import",
+    #         "tf2_msgs",
+    #         "geometry_msgs",
+    #     ],
+    #     output="screen",
+    #     respawn=True,
+    # )
+
+    # Relay - use instead of transform when Gazebo is only publishing odom -> base_link
+    topic_tools_tf = Node(
+        package="topic_tools",
+        executable="relay",
+        arguments=[
+            "/gz/tf",
+            "/tf",
+        ],
+        output="screen",
+        respawn=False,
+        condition=IfCondition(LaunchConfiguration("use_gz_tf")),
+    )
+
     return LaunchDescription(
         [
+            DeclareLaunchArgument(
+                "use_gz_tf", default_value="true", description="Use Gazebo TF."
+            ),
             sitl_dds,
             robot_state_publisher,
             bridge,
+            RegisterEventHandler(
+                OnProcessStart(
+                    target_action=bridge,
+                    on_start=[
+                        topic_tools_tf
+                    ]
+                )
+            ),
         ]
     )
